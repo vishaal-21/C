1b91983d541914882134b216db7649ff280699be
Vishal Puneyani <vishal.puneyani21@gmail.com>
2022-11-30 07:38:04 +0530

    Trees

---
+++ Trees/BTcount.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "BTfunctions.c"
+//--------------------FUNCTION TO CALCULATE HEIGHT OF BT-----------------------//
+int max(int a,int b)
+{
+    return a>b?a:b;
+}
+int height(Node* root)
+{
+    if(root==NULL)
+        return 0;
+    return (1+max(height(root->left),height(root->right)));
+}
+//-----------------------------------------------------------------------------//
+
+//--------------------FUNCTION TO COUNT NODES IN  BT-------------------------//
+int nodecount(Node* root)
+{
+    static int count=0;
+    if(root)
+    {
+        nodecount(root->left);
+        count++;
+        nodecount(root->right);
+    }
+    return count;
+}
+//-----------------------------------------------------------------------------//
+
+//--------------------FUNCTION TO COUNT LEAFS BT----------------------------//
+int leafcount(Node* root)
+{
+    static int count=0;
+    if(root)
+    {
+        leafcount(root->left);
+        if(root->left==NULL && root->right==NULL)
+            count++;
+        leafcount(root->right);
+    }
+    return count;
+}
+//-----------------------------------------------------------------------------//
+
+void main()
+{
+     Node* tree=NULL;
+
+    int root;
+    printf("Enter value of root node : ");
+    scanf("%d",&root);
+
+    tree=recursiveCreateBT(root);
+
+    printf("\nLevel order is : ");
+    levelorder(tree);
+
+    printf("\nHeight of tree is %d",height(tree));
+    printf("\nNumber of nodes tree is %d",nodecount(tree));
+    printf("\nNumber of leafs in tree is %d",leafcount(tree));
+}
